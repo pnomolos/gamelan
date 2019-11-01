f086b634569b6c3ce704bc82e05079d05818a254
Phil <pnomolos@gmail.com>
2019-11-01 15:48:46 -0700

    Use ruby PQ implementation

--- lib/gamelan/queue.rb
+++ lib/gamelan/queue.rb
@@ -1,27 +1,27 @@
 module Gamelan
 
   if jruby?
-    
+
     class Queue
       include Java
       include_package 'java.util'
-      
+
       def initialize(scheduler)
         @scheduler = scheduler
         @queue     = PriorityQueue.new(10000) { |a,b|
                        a.priority <=> b.priority
                      }
       end
-      
+
       def push(task)
         @queue.add(task)
       end
       alias << push
-      
+
       def pop
         @queue.remove
       end
-      
+
       def ready?
         if top = @queue.peek
           top.delay < @scheduler.phase
@@ -30,27 +30,24 @@ module Gamelan
         end
       end
     end
-    
+
   else
-    
-    require 'priority_queue/c_priority_queue'
-    require 'priority_queue'
-    
+
     class Queue
       def initialize(sched)
         @scheduler = sched
         @queue     = ::PriorityQueue.new
       end
-      
+
       def push(task)
         @queue.push(task, task.priority)
       end
       alias << push
-      
+
       def pop
         @queue.delete_min[0]
       end
-      
+
       def ready?
         if top = @queue.min
           top[1].time < @scheduler.phase
